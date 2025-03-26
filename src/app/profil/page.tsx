e967f77dab273c9d7b448ecff171325dcc397242
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-03-26 12:12:56 +0100

    Ajout des pages profile et setting

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -1,7 +1,16 @@
+"use client"
 import React from 'react'
+import Footer from '../components/Footer'
+import { UserAtom } from '../utils/atoms';
+import { useAtom } from 'jotai';
+
 
 export default function Profil() {
+  const [user] = useAtom(UserAtom);
   return (
-    <div>Profil</div>
+    <div>
+      <p>Profile</p>
+      <Footer/>
+    </div>
   )
 }
