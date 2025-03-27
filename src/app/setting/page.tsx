dec2c45eb1a358eb29919423c9da4f7995078917
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-03-27 12:18:44 +0100

    Ajout de la function updateUser a la page setting

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,17 +1,57 @@
 "use client"
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Avatar from '../components/Avatar'
 import Footer from '../components/Footer'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import { UserAtom } from '../utils/atoms'
 import { useAtomValue } from 'jotai'
+import { useMutation } from '@apollo/client'
+import { UPDATE_USER } from '@/services/updateUser'
 
 
 type Props = {}
 
 export default function Setting({}: Props) {
+  const token = localStorage.getItem("authToken")
   const userInfo = useAtomValue(UserAtom)
+  const [userUpdate, setUserUpdate] = useState({
+ mdp:"",
+ pseudo:""
+  })
+  useEffect(() => {
+    if(! userInfo){
+      console.warn("Les informations de l'utilisateur ne pas chargé")
+    }
+  }, [userInfo])
+  
+const [updateUser, {loading, data, error}] = useMutation(UPDATE_USER, {
+  context: {
+    headers : {
+      Authorization : `Bearer ${token}`
+    }
+  },
+  onCompleted(data){
+    alert("Profile mise à jour !")
+  }
+})
+  const handleUpdate = async(e: any)=>{
+    e.preventDefault()
+    if(!userInfo){
+      alert("Impossible de mettre à jour :  les informations utilisateur sont manquante")
+      return;
+    }
+    try {
+      await updateUser({
+        variables: {
+          updateUserId: userInfo.id,
+          input :userUpdate
+        }
+      })
+    } catch (error) {
+      console.error("Erreur lors de la mise  à jour ! :", error)
+    }
+  }
   return (
     <>
     <div className='flex flex-col items-center jutify-center  m-auto w-lg mt-10'>
@@ -22,11 +62,14 @@ export default function Setting({}: Props) {
       <p className='text-redpapilles'>Nom: {userInfo?.nom}</p>
       <p className='text-redpapilles'>E-mail: {userInfo?.email}</p>
       </div>
+      <form onSubmit={handleUpdate}>
       <div className='flex flex-col m-4  m-auto w-sm mt-5'>
-      <Input className='w-96' label='Pseudo' type='text' value='Pseudo' onChange={()=>console.log()} name='pseudo' id='pseudo' required={false}/>
-      <Input className='w-96' label='Mot de passe' type='password' value='Mot de passe' onChange={()=>console.log()} name='mdp' id='mdp' required={false}/>
-      <Button type='button' txt='Mettre à jours'onClick={()=>console.log("MAJ")} className='text-redpapilles w-60'/>
+      <Input className='w-96' label='Pseudo' type='text' value={userUpdate.pseudo} onChange={(e)=> setUserUpdate({...userUpdate, pseudo: e.target.value})} name='pseudo' id='pseudo' required={false}/>
+      <Input className='w-96' label='Mot de passe' type='password' value={userUpdate.mdp} onChange={(e)=> setUserUpdate({...userUpdate, mdp: e.target.value})} name='mdp' id='mdp' required={false}/>
+      <Button type='submit' txt='Mettre à jours'onClick={()=>console.log("MAJ")} className='text-redpapilles w-60'/>
       </div>
+      </form>
+
     </div>
       <Footer/>
     </>
