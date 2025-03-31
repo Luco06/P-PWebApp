451ce858f8d0e94aa0fe2851f9cd45dff9cd6624
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-03-31 17:07:16 +0200

    Format fichier

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,23 +1,42 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 type InputProps = {
-    label?: string
-    type: string
-    value?: string
-    name: string
-    id: string
-    autoComplete?:string
-    onChange: (e:React.ChangeEvent<HTMLInputElement>)=> void
-    className?: string;
-    required: boolean
-}
+  label?: string;
+  type: string;
+  value?: string;
+  name: string;
+  id: string;
+  autoComplete?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  required: boolean;
+};
 
-export default function Input({label, type, value, onChange, name, id, className="", required, autoComplete}: InputProps) {
+export default function Input({
+  label,
+  type,
+  value,
+  onChange,
+  name,
+  id,
+  className = "",
+  required,
+  autoComplete,
+}: InputProps) {
   return (
-    <div className="mt-4 m-auto">
-    <label className="block text-sm/6 text-colortxt">{label}</label>
-      <input autoComplete={autoComplete} type={type} value={value} onChange={onChange} name={name} id={id} required={required}  className={`drop-shadow-md block h-11 rounded-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-redpapilles sm:text-sm/6 ${className}`}/>
+    <div className="mt-4 m-auto items-center">
+      <label className="block text-sm/6 text-colortxt text-start">{label}</label>
+      <input
+        autoComplete={autoComplete}
+        type={type}
+        value={value}
+        onChange={onChange}
+        name={name}
+        id={id}
+        required={required}
+        className={`drop-shadow-md block h-11 rounded-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-redpapilles sm:text-sm/6 ${className}`}
+      />
     </div>
-  )
+  );
 }
