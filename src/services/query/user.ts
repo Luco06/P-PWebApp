e48271633c83b1b0ee8aa2e1fecf9372ba08d43d
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-04-01 16:14:36 +0200

    AJout des dossier mutation et query

---
+++ src/services/query/user.ts
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const GET_USER = gql`
+query User($userId: ID!) {
+  user(id: $userId) {
+    id
+    email
+    nom
+    prenom
+    avatar
+    pseudo
+    recettes {
+      id
+      titre
+      description
+      ingredients
+      tps_prep
+      tps_cook
+      nb_person
+      dificulty
+      est_public
+      cout
+      note
+      instructions
+      categorie
+      img
+      favoris
+      dateCreation
+    }
+  }
+}
+`
