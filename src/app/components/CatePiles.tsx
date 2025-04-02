a53c73ff4cf31f7a80bad0a06fc2574beff8cad1
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-04-02 17:30:09 +0200

    AJout de la props

--- src/app/components/CatePiles.tsx
+++ src/app/components/CatePiles.tsx
@@ -1,44 +1,35 @@
-"use client"
-import React from 'react'
-import Button from './Button'
-/**Ajouter les Props */
-type Props = {}
+import React from "react";
+import Button from "./Button";
 
-export default function CatePiles({}: Props) {
+type Props = {
+  onSelectCategory: (category: string) => void;
+  selectedCategory: string | null;
+};
+
+export default function CatePiles({
+  onSelectCategory,
+  selectedCategory,
+}: Props) {
   const Categories = [
-    {
-    id:1,
-    txt:"Viande"
-  },
-  {
-    id:2,
-    txt:"Poisson"
-  },
-  {
-    id:3,
-    txt:"Végétarien"
-  },
-  {
-    id:4,
-    txt:"Desserts"
-  },
-  {
-    id:5,
-    txt:"Boissons"
-  },
-  {
-    id:6,
-    txt:"Sauces"
-  },
-  ]
+    { id: 1, txt: "Viande" },
+    { id: 2, txt: "Poisson" },
+    { id: 3, txt: "Végétarien" },
+    { id: 4, txt: "Desserts" },
+    { id: 5, txt: "Boissons" },
+    { id: 6, txt: "Sauces" },
+  ];
+
   return (
-    <>
-   <div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-5xl'>
-    {Categories.map((categorie)=>(
-      <Button key={categorie.id} type='button' txt={categorie.txt} className='w-40'/>
-    ))}
+    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-5xl">
+      {Categories.map((categorie) => (
+        <Button
+          key={categorie.id}
+          txt={categorie.txt}
+          className="w-40"
+          active={selectedCategory === categorie.txt}
+          onClick={() => onSelectCategory(categorie.txt)}
+        />
+      ))}
     </div>
-    </>
-  
-  )
+  );
 }
