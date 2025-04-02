de01b581c623753f59d6c98c59cda575f0b6cec3
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-04-02 17:29:18 +0200

    Ajout de la props active

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,16 +1,30 @@
-"use client"
-
-import React, {ReactNode} from 'react'
+"use client";
+import React, { ReactNode } from "react";
 
 type ButtonProps = {
-    txt: string | ReactNode;
-    className?: string;
-    type?: "submit" | "reset" | "button";
-    onClick?: ()=>void;
-}
+  txt: string | ReactNode;
+  className?: string;
+  type?: "submit" | "reset" | "button";
+  onClick?: () => void;
+  active?: boolean;
+};
 
-export default function Button({type, txt, className="", onClick}: ButtonProps) {
+export default function Button({
+  type = "button",
+  txt,
+  className = "",
+  onClick,
+  active = false,
+}: ButtonProps) {
   return (
-    <button type={type} onClick={onClick} className={`drop-shadow-md flex flex-row justify-center items-center gap-3 cursor-pointer gap- block  m-auto mt-4 rounded-full bg-white px-3.5 py-2.5 text-center text-sm font-semibold text-colortxt shadow-xs  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${className}`}>{txt}</button>
-  )
+    <button
+      type={type}
+      onClick={onClick}
+      className={`drop-shadow-md flex flex-row justify-center items-center gap-3 cursor-pointer block m-auto mt-4 rounded-full px-3.5 py-2.5 text-center text-sm font-semibold shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
+        active ? "bg-redpapilles text-white" : "bg-white text-colortxt"
+      } ${className}`}
+    >
+      {txt}
+    </button>
+  );
 }
