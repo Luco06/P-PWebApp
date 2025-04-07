9a607ad62b419e45930ab1319552a56709bfa808
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-04-07 17:32:55 +0200

    AJout de la function createComment

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,32 +5,34 @@ import { UserAtom } from "../utils/atoms";
 import { useAtom } from "jotai";
 import Footer from "../components/Footer";
 import { RecipeType } from "../utils/atoms";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { GET_RECIPE } from "@/services/query/recipe";
+import { ADD_COMMENT } from "@/services/mutations/AddComment";
 import CardRecipesprofile from "../components/CardRecipesProfile";
 import CardRecipe from "../components/CardRecipe";
 import Button from "../components/Button";
 import Comments from "../components/Comments";
-import { formatDistanceToNow } from 'date-fns';
-import {fr} from 'date-fns/locale'
+import Input from "../components/Input";
+import { formatDistanceToNow } from "date-fns";
+import { fr } from "date-fns/locale";
 
 export default function Acceuil() {
-
   const formatDate = (timestamp: string): string => {
     const dateInMillis = parseInt(timestamp, 10); // Convertir en nombre
     if (isNaN(dateInMillis)) {
       return "Date invalide"; // Gérer le cas où la conversion échoue
     }
-    
+
     const date = new Date(dateInMillis); // Créer un objet Date à partir du timestamp
-    return formatDistanceToNow(date, { addSuffix: true,locale:fr });
+    return formatDistanceToNow(date, { addSuffix: true, locale: fr });
   };
-  
-  
+
   const [user] = useAtom(UserAtom);
   const [recipes, setRecipes] = useState<RecipeType[]>([]);
+  const [token, setToken] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState<RecipeType | null>(null);
+  const [commentaire, setCommentaire] = useState("");
 
   const { data } = useQuery(GET_RECIPE);
   useEffect(() => {
@@ -40,13 +42,49 @@ export default function Acceuil() {
       );
       setRecipes(publicRecipes);
     }
-  }, [data, setRecipes]);
+    const storedToken = localStorage.getItem("authToken");
+    if (!user || !storedToken) {
+      setToken(null);
+      return;
+    }
+    setToken(storedToken);
+  }, [data, setRecipes, user]);
+
+  const [CreateComment, { error }] = useMutation(ADD_COMMENT, {
+    context: {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+    refetchQueries: [{ query: GET_RECIPE }],
+    onCompleted: () => {
+      alert("Commentaire ajouté !");
+      setCommentaire("");
+    },
+  });
+  
+  const handleAddComment = () => {
+    if (!user?.id || !selectedRecipe?.id || !commentaire.trim()) {
+      alert("Le commentaire ne peut pas être vide.");
+      return;
+    }
+    CreateComment({
+      variables: {
+        input: {
+          auteur: user.id,
+          recette: selectedRecipe.id,
+          contenu: commentaire.trim(),
+        },
+      },
+    });
+  };
 
   const handleRecipeClick = (recipe: RecipeType) => {
     setSelectedRecipe(recipe);
     setIsModalOpen(true);
   };
   console.log(recipes);
+  console.log(commentaire, "Commentaire");
   return (
     <div>
       <Header
@@ -91,16 +129,40 @@ export default function Acceuil() {
                   txt="Fermer"
                 />
               </div>
-              <h4 className="text-center">Commentaire</h4>
-              {selectedRecipe?.commentaire.map((comment) => (
-                <Comments
-                  key={comment?.id}
-                  avatar={comment?.auteur.avatar}
-                  prenom={comment?.auteur.prenom}
-                  contenu={comment?.contenu}
-                  date={formatDate(comment?.dateCreation)}
-                />
-              ))}
+              <div>
+                <h4 className="text-center">Commentaire</h4>
+                {selectedRecipe?.commentaire.map((comment) => (
+                  <Comments
+                    key={comment?.id}
+                    avatar={comment?.auteur.avatar}
+                    prenom={comment?.auteur.prenom}
+                    contenu={comment?.contenu}
+                    date={formatDate(comment?.dateCreation)}
+                  />
+                ))}
+                {user && (
+                  <>
+                    <Input
+                      label=""
+                      type="text"
+                      value={commentaire}
+                      name="commentaire"
+                      id="commentaire"
+                      required={true}
+                      onChange={(e) => setCommentaire(e.target.value)}
+                      className="w-96 m-auto"
+                    />
+                    <Button
+                      disabled={!commentaire.trim()}
+                      onClick={handleAddComment}
+                      type="button"
+                      className="text-redpapilles w-50 border boder-redpapilles"
+                      txt="Envoyer"
+                      
+                    />
+                  </>
+                )}
+              </div>
             </div>
           </div>
         )}
